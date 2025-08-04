2a0c199bfc9fecae672227448dad2b3a3261eefa
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-08-05 01:09:56 +0530

    create app

---
+++ src/main/java/com/example/Mobile_phone_ecommerce_backend/MobilePhoneEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mobile_phone_ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePhoneEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePhoneEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mobile_phone_ecommerce_backend/MobilePhoneEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mobile_phone_ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePhoneEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
